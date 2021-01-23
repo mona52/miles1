92028f44454ec23d165dd2eeb7175a51d161855b
mona52 <mona52@list.ru>
2021-01-23 09:36:16 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticketPrice = 35_554;
+        long normOfMiles = 20;
+        long miles = ticketPrice / normOfMiles;
+        System.out.println(miles);
+    }
+}
